f355495932ed9ae204952271a2ee70485a286de4
AlexKomanov <akomanov88@gmail.com>
2023-10-02 19:48:45 +0300

    adding tickets fetching

---
+++ app/tickets/TicketsList.jsx
@@ -0,0 +1,32 @@
+const getTickets = async () => {
+    const res = await fetch("http://localhost:4000/tickets",{
+        next: {
+            revalidate: 0 // use 0 to cancel cache usage
+        }
+    })
+
+    return res.json()
+}
+
+const TicketsList = async () => {
+    const tickets = await getTickets()
+    return (
+        <>
+            {tickets.length === 0 && (
+                <p className="text-center">There are no open tickets! :)</p>
+            )}
+            {tickets.map((ticket) => (
+                <div key={ticket.id} className="card my-5">
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)} ...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </div>
+            ))}
+
+        </>
+    );
+};
+
+export default TicketsList;
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import TicketsList from "@/app/tickets/TicketsList";
 
 const Tickets = () => {
     return (
         <main>
+            <nav>
             <h2>Tickets</h2>
+                <p><small>Currently open tickets.</small></p>
+            </nav>
+            <TicketsList />
         </main>
     );
 };
