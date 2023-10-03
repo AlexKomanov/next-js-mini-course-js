c62175471294128df3aed77177f03573043c0a30
AlexKomanov <akomanov88@gmail.com>
2023-10-03 10:55:40 +0300

    adding dynamic segment by ticket id

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const getTickets = async () => {
     const res = await fetch("http://localhost:4000/tickets",{
         next: {
@@ -17,11 +19,13 @@ const TicketsList = async () => {
             )}
             {tickets.map((ticket) => (
                 <div key={ticket.id} className="card my-5">
+                    <Link href={`/tickets/${ticket.id}`}>
                     <h3>{ticket.title}</h3>
                     <p>{ticket.body.slice(0, 200)} ...</p>
                     <div className={`pill ${ticket.priority}`}>
                         {ticket.priority} priority
                     </div>
+                    </Link>
                 </div>
             ))}
 
---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,32 @@
+const getTicket = async (id) => {
+    const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+        next: {
+            revalidate: 30
+        }
+    })
+
+    return res.json()
+}
+
+const TicketDetails = async ({params}) => {
+    const ticket = await getTicket(params.id)
+    return (
+        <main>
+            <nav>
+                <h2>Ticket Details</h2>
+            </nav>
+
+            <div className="card">
+                <h3>{ticket.title}</h3>
+                <small>Created by {ticket.user_email}</small>
+                <p>{ticket.body}</p>
+                <div className={`pill ${ticket.priority}`}>
+                    {ticket.priority} priority
+                </div>
+
+            </div>
+        </main>
+    );
+};
+
+export default TicketDetails;
