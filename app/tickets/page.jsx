d8e4731bf586964e62af04deca29f07d0b851ad6
AlexKomanov <akomanov88@gmail.com>
2023-10-04 07:14:46 +0300

    Adding loading component

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import TicketsList from "@/app/tickets/TicketsList";
+import Loading from "@/app/loading";
 
 const Tickets = () => {
     return (
         <main>
             <nav>
-            <h2>Tickets</h2>
+                <h2>Tickets</h2>
                 <p><small>Currently open tickets.</small></p>
             </nav>
-            <TicketsList />
+
+            <Suspense fallback={<Loading />}>
+                <TicketsList/>
+            </Suspense>
         </main>
     );
 };
